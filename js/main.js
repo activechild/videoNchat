7664f4dc3be8a43bce822d0aea22cb0a9c933945
blackwidowmaker <jtptillman@gmail.com>
2015-07-15 07:16:51 -0500

    basic framework and functionality added
    added html5 video and parts of ajax chat functionality

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var theVideo = document.getElementsById("video");
+
+function large() {
+    theVideo.width = 854;
+}
+
+function small() {
+    theVideo.width = 320;
+}
+
+function medium() {
+    theVideo.width = 420;
+}
